66f473b7fa64d94867a61b7fff1c397f966c7d6e
Aldo Ziflaj <aldoziflaj95@gmail.com>
2021-03-26 21:17:49 +0100

    init tested more

--- test_suite/spec/init_spec.rb
+++ test_suite/spec/init_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe 'Initialize new Gogot repository' do
+RSpec.describe 'Initializing a new Gogot repository' do
   let!(:command_result) { `#{command}` }
 
   context 'without a path' do
@@ -9,14 +9,33 @@ RSpec.describe 'Initialize new Gogot repository' do
     end
   end
 
-  context 'with missing directory' do
+  context 'with a new directory' do
     let(:command) { %(gogot init kewl-projekt) }
 
-    before { system('rm -rf ./kewl-projekt') }
+    around do |example|
+      `rm -rf ./kewl-projekt`
+      example.run
+      `rm -rf ./kewl-projekt` # to clean up
+    end
 
     it 'creates the directory if missing' do
       expect(command_result).to include('Initalizing new Gogot repo')
       expect(command_result).to include('Gogot repo initialized in kewl-projekt/.gogot')
+    end
+  end
+
+  context 'with an empty directory' do
+    let(:command) { %(gogot init projekt) }
+
+    around do |example|
+      `mkdir projekt`
+      example.run
+      `rm -rf projekt`
+    end
+
+    it 'initializes the repo anyway' do
+      expect(command_result).to include('Initalizing new Gogot repo')
+      expect(command_result).to include('Gogot repo initialized in projekt/.gogot')
     end
   end
 end
